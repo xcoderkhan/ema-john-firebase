954feaeacb2c64a9b2a776e64841284687a16aa1
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-06-26 06:57:37 +0600

    Login page Update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import './Login.jsx'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
     return (
         <>
-            <h2>Login coming sooon</h2>
             <section className="bg-gray-50 min-h-screen flex items-center justify-center">
                 <div className="bg-red-300 flex rounded-2xl shadow-lg max-w-3xl">
                     <div className="sm:w-1/2 text-green-800 px-10 pt-5">
                         <h2 className="font-bold text-2x">Login</h2>
-                        <p className="text-sm mt-2">If you already register, Login here!!</p>
+                        <p className="text-sm mt-1">If you already register, Login here!!</p>
                         <form action="#" className="flex flex-col gap-3">
-                            <input type="email" name="email" placeholder="Input your email" required className="p-2 mt-8 rounded-xl border" />
-                            <input type="password" name="password" placeholder="Input your password" className="p-2 rounded-xl border" />
-                            <button type="submit" className="bg-cyan-300 text-white">Login</button>
+                            <input type="email" name="email" placeholder="Input your email" required className="p-2 mt-5 rounded-xl border" />
+                            <div className=" relative">
+                                <input type="password" name="password" placeholder="Input your password" className="p-2 rounded-xl border w-full" />
+                                <>
+                                    <FontAwesomeIcon icon={faEye} className=" absolute top-1/2 right-3 -translate-y-1/2" />
+                                </>
+                            </div>
+                            <button type="submit" className="bg-cyan-300 text-white rounded-xl py-2">Login</button>
                         </form>
 
+                        <div className="flex items-center justify-between mt-2">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">If you need to change:</label>
+                            <div className="text-sm">
+                                <Link to="#" className="font-bold text-cyan-300 hover:text-red-200">
+                                    Forget Password
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="mt-3 grid grid-cols-3 items-center text-sm text-green-800">
+                            <hr className=" border-green-800" />
+                            <p className="text-center">Or</p>
+                            <hr className=" border-green-800" />
+                        </div>
+                        <button type="submit" className=" mt-3 bg-cyan-300 text-white rounded-xl py-2 w-full">Login with Google</button>
                     </div>
                     <div className="w-1/2 p-5 sm:block hidden">
                         <img src="/src/images/login/login.jpg" alt="Login Image" className="rounded-2xl" />
                     </div>
                 </div>
-
             </section>
 
 
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -20,14 +20,7 @@ const SignUp = () => {
                                 </div>
                             </div>
                             <div>
-                                <div className="flex items-center justify-between">
-                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                    <div className="text-sm">
-                                        <Link to="#" className="font-bold text-cyan-300 hover:text-red-200">
-                                            Forget Password
-                                        </Link>
-                                    </div>
-                                </div>
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
                                 <div className="mt-2">
                                     <input type="password" name="password" autoCapitalize="current-password" placeholder="Input your password" required className="block w-full rounded-md border-0 py-1 5 text-gray-900 shadow-sm ring-1 ring-insert ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-200 sm:text-sm sm:leading-6" />
                                 </div>
