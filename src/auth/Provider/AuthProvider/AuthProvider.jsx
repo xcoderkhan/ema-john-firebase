bab26aca248f3a2db77239d91f6361403e69567c
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-06-27 08:18:44 +0600

    U

--- src/auth/Provider/AuthProvider/AuthProvider.jsx
+++ src/auth/Provider/AuthProvider/AuthProvider.jsx
@@ -1,7 +1,11 @@
 import React, { createContext } from 'react';
+import { getAuth } from "firebase/auth";
+import app from '../../Firebase/firebase.config';
 
 
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
 
